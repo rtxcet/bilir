f934ad06be23f9dc46b39f0760d7ab72fdb3029e
rtxcet <cetinrtx@gmail.com>
2025-10-21 20:51:08 +0300

    Start 01

---
+++ Components/Footer/Footer/FooterViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bilir.Components
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
---
+++ Components/Header/HeaderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bilir.Components.Header
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
